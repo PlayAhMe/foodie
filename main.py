1f2ecfa20750fa4f87b5bd80ddabf7dcfb356ce8
Ashley <cssi@cssis-MacBook-Pro.local>
2019-07-23 12:21:28 -0400

    Adding handlers

---
+++ main.py
@@ -0,0 +1,31 @@
+import webapp2
+
+class Home(webapp2.RequestHandler):
+    def get(self):
+        home_template = jinja_env.get_template('home.html')
+
+class Filters(webapp2.RequestHandler):
+    def get(self):
+        filters_template = jinja_env.get_template('filters.html')
+
+class RestaurantsNearby(webapp2.RequestHandler):
+    def get(self):
+        restaurants_nearby_template = jinja_env.get_template('restaurants_nearby.html')
+
+class Summary(webapp2.RequestHandler):
+    def get(self):
+        summary_template = jinja_env.get_template('summary.html')
+
+class Restaurant(webapp2.RequestHandler):
+    def get(self):
+        restaurant_template = jinja_env.get_template('restaurant.html')
+
+app = webapp2.WSGIApplication(
+    [
+        ('/', Home),
+        ('/filters', Filters),
+        ('/restaurants_nearby', RestaurantsNearby),
+        ('/summary', Summary),
+        ('/restaurant', Restaurant),
+    ],
+    debug = True
