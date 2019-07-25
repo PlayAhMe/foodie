a01bf18f6a317a9bdab4b621d081f78d32ccad93
Juan <juanparamo01@gmail.com>
2019-07-25 10:58:35 -0700

    improved hpme page

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-function httpGet(){
-    console.log("de")
-
-    var theUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=5&type=restaurant&keyword=Mexican&key=AIzaSyDGnMTSopj_ZzyiNWEEM_pdb6tBCHYxEc8"
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.withCredentials = false;
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-
-    console.log(xmlHttp.responseText)
-    return xmlHttp.responseText;
-}
-
-httpGet()
+$(document).ready(function(){
+  $('.header').height($(window).height());
+})
